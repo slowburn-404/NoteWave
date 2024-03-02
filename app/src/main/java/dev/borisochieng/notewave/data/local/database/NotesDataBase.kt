5f91d2775db118e3fccd1bb75552614013fe866f
slowburn-404 <borisochola32@gmail.com>
2024-03-02 23:41:20 +0300

    added auto migration

--- app/src/main/java/dev/borisochieng/notewave/data/local/database/NotesDataBase.kt
+++ app/src/main/java/dev/borisochieng/notewave/data/local/database/NotesDataBase.kt
@@ -1,21 +1,28 @@
 package dev.borisochieng.notewave.data.local.database
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import dev.borisochieng.notewave.data.models.Note
+import dev.borisochieng.notewave.data.utils.DateConverters
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.internal.synchronized
 import kotlin.concurrent.Volatile
 
 @Database(
     entities = [Note::class],
-    version = 2,
-    exportSchema = false,
+    version = 4,
+    exportSchema = true,
+   // autoMigrations = [
+      //  AutoMigration(from = 1, to = 4)
+    //]
 )
+@TypeConverters(DateConverters::class)
 abstract class NotesDataBase : RoomDatabase() {
 
     abstract fun notesDAO(): NotesDao
@@ -39,13 +46,21 @@ abstract class NotesDataBase : RoomDatabase() {
 
         }
 
+        private val MIGRATION_3_4: Migration = object : Migration(3, 4) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE notes_table RENAME COLUMN updated_at to time_stamp")
+
+            }
+
+        }
+
         @OptIn(InternalCoroutinesApi::class)
         fun getDatabase(context: Context): NotesDataBase {
             //if instance is not null return the DB if not create it
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext, NotesDataBase::class.java, "notes_database"
-                ).addMigrations(MIGRATION_1_2, MIGRATION_2_3).build()
+                ).addMigrations(MIGRATION_2_3, MIGRATION_3_4).build()
                 INSTANCE = instance
 
                 //return instance
