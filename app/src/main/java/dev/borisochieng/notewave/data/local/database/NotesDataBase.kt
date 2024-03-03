f4dd31b879d3e8dbc33f85a7b14eae06ee553ab3
slowburn-404 <borisochola32@gmail.com>
2024-03-03 11:40:58 +0300

    added room db auto migration, changed and exported the database schema

--- app/src/main/java/dev/borisochieng/notewave/data/local/database/NotesDataBase.kt
+++ app/src/main/java/dev/borisochieng/notewave/data/local/database/NotesDataBase.kt
@@ -18,9 +18,9 @@ import kotlin.concurrent.Volatile
     entities = [Note::class],
     version = 4,
     exportSchema = true,
-   // autoMigrations = [
-      //  AutoMigration(from = 1, to = 4)
-    //]
+    autoMigrations = [
+        AutoMigration(from = 3, to = 4)
+    ]
 )
 @TypeConverters(DateConverters::class)
 abstract class NotesDataBase : RoomDatabase() {
@@ -31,36 +31,13 @@ abstract class NotesDataBase : RoomDatabase() {
         @Volatile
         private var INSTANCE: NotesDataBase? = null
 
-        private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
-            override fun migrate(db: SupportSQLiteDatabase) {
-                db.execSQL("CREATE TABLE IF NOT EXISTS notes_table ( note_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL, updated_at TEXT NOT NULL);")
-
-            }
-
-        }
-        private val MIGRATION_2_3: Migration = object : Migration(2, 3) {
-            override fun migrate(db: SupportSQLiteDatabase) {
-                db.execSQL("CREATE TABLE IF NOT EXISTS notes_table ( note_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL, updated_at TEXT NOT NULL);")
-
-            }
-
-        }
-
-        private val MIGRATION_3_4: Migration = object : Migration(3, 4) {
-            override fun migrate(db: SupportSQLiteDatabase) {
-                db.execSQL("ALTER TABLE notes_table RENAME COLUMN updated_at to time_stamp")
-
-            }
-
-        }
-
         @OptIn(InternalCoroutinesApi::class)
         fun getDatabase(context: Context): NotesDataBase {
             //if instance is not null return the DB if not create it
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext, NotesDataBase::class.java, "notes_database"
-                ).addMigrations(MIGRATION_2_3, MIGRATION_3_4).build()
+                ).build()
                 INSTANCE = instance
 
                 //return instance
